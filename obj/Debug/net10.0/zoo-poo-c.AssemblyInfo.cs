9f65f90286816ab5c6de4e48f70e689dfecd2404
enzoandria1-oss <enzoandria1@gmail.com>
2026-02-17 13:55:59 +0100

    feat

--- obj/Debug/net10.0/zoo-poo-c.AssemblyInfo.cs
+++ obj/Debug/net10.0/zoo-poo-c.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zoo-poo-c")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b36a5e808dc4b1a5186d8878f7c306aa48bb34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b71776a9edca9a2e2eb4e012a78d1527fa8ee75")]
 [assembly: System.Reflection.AssemblyProductAttribute("zoo-poo-c")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zoo-poo-c")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
